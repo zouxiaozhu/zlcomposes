0e69ebcfac2bf3b9c7f77f03c0acfe628bac13c0
zhanglong <1428804176@qq.com>
2018-12-06 19:51:51 +0800

    init

---
+++ src/Client.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanglong
+ * Date: 2018/12/6
+ * Time: 下午6:39
+ */
+
+namespace Zlcompose\MSQ;
+
+class Client
+{
+    public function test()
+    {
+        echo 111;die;
+    }
+}
